07a135909489e85bd540f90c49071b554bbdedc4
Hyun-EG <codiee@naver.com>
2025-03-31 19:20:30 +0900

    Chore: 초기 개발환경 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// PWA 설정 추가
+const nextConfig: NextConfig = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "google-fonts",
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+        },
+      },
+    },
+  ],
+});
 
 export default nextConfig;
