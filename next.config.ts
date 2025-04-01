148478a7bc9cd4338be6619e4519236f31d25902
seonghyun <codiee@naver.com>
2025-04-02 02:29:37 +0900

    Chore: 빌드에러 수정

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["openweathermap.org"], // HTTPS로 도메인 설정
-  },
-};
+const nextConfig = {};
 
 module.exports = withPWA({
   ...nextConfig,
