b90f1e53c5e6cb2104eb26716b8cc56b7836c076
seonghyun <codiee@naver.com>
2025-04-02 02:22:28 +0900

    Feat: 라우터 연결 및 Nav아이템 추가 (날씨)

---
+++ components/layout/Header.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const Header = () => {
+  const navItems = [
+    { nav: "홈", page: "/" },
+    { nav: "날씨", page: "/weather" },
+  ];
+  const router = useRouter();
+  return (
+    <div className="w-full h-20 border-b border-[#afafaf]">
+      <div className="h-10 flex justify-center items-center">PPOPPIS</div>
+      <div className="h-10 px-2.5 flex justify-start items-center gap-2.5">
+        {navItems.map((item, index) => (
+          <div
+            onClick={() => {
+              router.push(item.page);
+            }}
+            className="cursor-pointer"
+            key={index}
+          >
+            {item.nav}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
